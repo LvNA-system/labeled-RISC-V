1a2e6b8670adaf7042ddf8924404122bb53fe2fa
yzh <yuzihao@ict.ac.cn>
2016-12-24 11:55:09 +0800

    srcs,rtl,rocket: modify code to match .v generated by rocketchip repo
    * AXI bundle definition
     * remove arregion and awregion
     * modify awid, arid, bid, rid from 5-bit to 4-bit
     * remove wid
     * add 16-bit aruser and awuser
    * modify top-level module name
    * add CDMA bundle port
    * modify the signal names of mem bundle and mmio bundle
    * modify the signal name of interrupts

--- srcs/rtl/rocket/rocketchip_top.v
+++ srcs/rtl/rocket/rocketchip_top.v
@@ -34,21 +34,21 @@
 	`axi_single_signal(interface_name, AWCACHE, dir2, 4, prefix, aw_bits_cache), \
 	`axi_single_signal(interface_name, AWPROT, dir2, 3, prefix, aw_bits_prot), \
 	`axi_single_signal(interface_name, AWQOS, dir2, 4, prefix, aw_bits_qos), \
-	`axi_single_signal(interface_name, AWREGION, dir2, 4, prefix, aw_bits_region), \
-	`axi_single_signal(interface_name, AWID, dir2, 5, prefix, aw_bits_id), \
-	`axi_single_signal(interface_name, AWUSER, dir2, 1, prefix, aw_bits_user), \
+	/*`axi_single_signal(interface_name, AWREGION, dir2, 4, prefix, aw_bits_region), */\
+	`axi_single_signal(interface_name, AWID, dir2, 4, prefix, aw_bits_id), \
+	`axi_single_signal(interface_name, AWUSER, dir2, 16, prefix, aw_bits_user), \
 	`axi_single_signal(interface_name, WREADY, dir1, 1, prefix, w_ready), \
 	`axi_single_signal(interface_name, WVALID, dir2, 1, prefix, w_valid), \
 	`axi_single_signal(interface_name, WDATA, dir2, 64, prefix, w_bits_data), \
 	`axi_single_signal(interface_name, WLAST, dir2, 1, prefix, w_bits_last), \
-	`axi_single_signal(interface_name, WID, dir2, 5, prefix, w_bits_id), \
+	/*`axi_single_signal(interface_name, WID, dir2, 5, prefix, w_bits_id), */\
 	`axi_single_signal(interface_name, WSTRB, dir2, 8, prefix, w_bits_strb), \
-	`axi_single_signal(interface_name, WUSER, dir2, 1, prefix, w_bits_user), \
+	/*`axi_single_signal(interface_name, WUSER, dir2, 1, prefix, w_bits_user), */\
 	`axi_single_signal(interface_name, BREADY, dir2, 1, prefix, b_ready), \
 	`axi_single_signal(interface_name, BVALID, dir1, 1, prefix, b_valid), \
 	`axi_single_signal(interface_name, BRESP, dir1, 2, prefix, b_bits_resp), \
-	`axi_single_signal(interface_name, BID, dir1, 5, prefix, b_bits_id), \
-	`axi_single_signal(interface_name, BUSER, dir1, 1, prefix, b_bits_user), \
+	`axi_single_signal(interface_name, BID, dir1, 4, prefix, b_bits_id), \
+	/*`axi_single_signal(interface_name, BUSER, dir1, 1, prefix, b_bits_user), */\
 	`axi_single_signal(interface_name, ARREADY, dir1, 1, prefix, ar_ready), \
 	`axi_single_signal(interface_name, ARVALID, dir2, 1, prefix, ar_valid), \
 	`axi_single_signal(interface_name, ARADDR, dir2, 32, prefix, ar_bits_addr), \
@@ -59,16 +59,16 @@
 	`axi_single_signal(interface_name, ARCACHE, dir2, 4, prefix, ar_bits_cache), \
 	`axi_single_signal(interface_name, ARPROT, dir2, 3, prefix, ar_bits_prot), \
 	`axi_single_signal(interface_name, ARQOS, dir2, 4, prefix, ar_bits_qos), \
-	`axi_single_signal(interface_name, ARREGION, dir2, 4, prefix, ar_bits_region), \
-	`axi_single_signal(interface_name, ARID, dir2, 5, prefix, ar_bits_id), \
-	`axi_single_signal(interface_name, ARUSER, dir2, 1, prefix, ar_bits_user), \
+	/*`axi_single_signal(interface_name, ARREGION, dir2, 4, prefix, ar_bits_region), */\
+	`axi_single_signal(interface_name, ARID, dir2, 4, prefix, ar_bits_id), \
+	`axi_single_signal(interface_name, ARUSER, dir2, 16, prefix, ar_bits_user), \
 	`axi_single_signal(interface_name, RREADY, dir2, 1, prefix, r_ready), \
 	`axi_single_signal(interface_name, RVALID, dir1, 1, prefix, r_valid), \
 	`axi_single_signal(interface_name, RRESP, dir1, 2, prefix, r_bits_resp), \
 	`axi_single_signal(interface_name, RDATA, dir1, 64, prefix, r_bits_data), \
 	`axi_single_signal(interface_name, RLAST, dir1, 1, prefix, r_bits_last), \
-	`axi_single_signal(interface_name, RID, dir1, 5, prefix, r_bits_id), \
-	`axi_single_signal(interface_name, RUSER, dir1, 1, prefix, r_bits_user)
+	`axi_single_signal(interface_name, RID, dir1, 4, prefix, r_bits_id) \
+	/*`axi_single_signal(interface_name, RUSER, dir1, 1, prefix, r_bits_user) */
 
 `define axi_out_interface(interface_name, prefix) \
 	`axi_interface(interface_name, input, output, prefix)
@@ -90,21 +90,21 @@
 	`axi_connect_single_signal(io_prefix, wire_prefix, aw_bits_cache), \
 	`axi_connect_single_signal(io_prefix, wire_prefix, aw_bits_prot), \
 	`axi_connect_single_signal(io_prefix, wire_prefix, aw_bits_qos), \
-	`axi_connect_single_signal(io_prefix, wire_prefix, aw_bits_region), \
+	/*`axi_connect_single_signal(io_prefix, wire_prefix, aw_bits_region), */\
 	`axi_connect_single_signal(io_prefix, wire_prefix, aw_bits_id), \
 	`axi_connect_single_signal(io_prefix, wire_prefix, aw_bits_user), \
 	`axi_connect_single_signal(io_prefix, wire_prefix, w_ready), \
 	`axi_connect_single_signal(io_prefix, wire_prefix, w_valid), \
 	`axi_connect_single_signal(io_prefix, wire_prefix, w_bits_data), \
 	`axi_connect_single_signal(io_prefix, wire_prefix, w_bits_last), \
-	`axi_connect_single_signal(io_prefix, wire_prefix, w_bits_id), \
+	/*`axi_connect_single_signal(io_prefix, wire_prefix, w_bits_id), */\
 	`axi_connect_single_signal(io_prefix, wire_prefix, w_bits_strb), \
-	`axi_connect_single_signal(io_prefix, wire_prefix, w_bits_user), \
+	/*`axi_connect_single_signal(io_prefix, wire_prefix, w_bits_user), */\
 	`axi_connect_single_signal(io_prefix, wire_prefix, b_ready), \
 	`axi_connect_single_signal(io_prefix, wire_prefix, b_valid), \
 	`axi_connect_single_signal(io_prefix, wire_prefix, b_bits_resp), \
 	`axi_connect_single_signal(io_prefix, wire_prefix, b_bits_id), \
-	`axi_connect_single_signal(io_prefix, wire_prefix, b_bits_user), \
+	/*`axi_connect_single_signal(io_prefix, wire_prefix, b_bits_user), */\
 	`axi_connect_single_signal(io_prefix, wire_prefix, ar_ready), \
 	`axi_connect_single_signal(io_prefix, wire_prefix, ar_valid), \
 	`axi_connect_single_signal(io_prefix, wire_prefix, ar_bits_addr), \
@@ -115,7 +115,7 @@
 	`axi_connect_single_signal(io_prefix, wire_prefix, ar_bits_cache), \
 	`axi_connect_single_signal(io_prefix, wire_prefix, ar_bits_prot), \
 	`axi_connect_single_signal(io_prefix, wire_prefix, ar_bits_qos), \
-	`axi_connect_single_signal(io_prefix, wire_prefix, ar_bits_region), \
+	/*`axi_connect_single_signal(io_prefix, wire_prefix, ar_bits_region), */\
 	`axi_connect_single_signal(io_prefix, wire_prefix, ar_bits_id), \
 	`axi_connect_single_signal(io_prefix, wire_prefix, ar_bits_user), \
 	`axi_connect_single_signal(io_prefix, wire_prefix, r_ready), \
@@ -123,14 +123,14 @@
 	`axi_connect_single_signal(io_prefix, wire_prefix, r_bits_resp), \
 	`axi_connect_single_signal(io_prefix, wire_prefix, r_bits_data), \
 	`axi_connect_single_signal(io_prefix, wire_prefix, r_bits_last), \
-	`axi_connect_single_signal(io_prefix, wire_prefix, r_bits_id), \
-	`axi_connect_single_signal(io_prefix, wire_prefix, r_bits_user)
+	`axi_connect_single_signal(io_prefix, wire_prefix, r_bits_id) \
+	/*`axi_connect_single_signal(io_prefix, wire_prefix, r_bits_user) */
 
 module rocketchip_top(
   input coreclk,
   input corerst,
 	(* X_INTERFACE_INFO = "xilinx.com:signal:clock:1.0 uncoreclk CLK" *)
-	(* X_INTERFACE_PARAMETER = "ASSOCIATED_BUSIF M_AXI_MEM:M_AXI_MMIO, ASSOCIATED_RESET uncorerst, FREQ_HZ 80000000" *)
+	(* X_INTERFACE_PARAMETER = "ASSOCIATED_BUSIF M_AXI_MEM:M_AXI_MMIO:M_AXI_CDMA, ASSOCIATED_RESET uncorerst, FREQ_HZ 80000000" *)
   input   uncoreclk,
   (* X_INTERFACE_INFO = "xilinx.com:signal:reset:1.0 uncorerst RST" *)
   (* X_INTERFACE_PARAMETER = "POLARITY ACTIVE_HIGH" *)
@@ -138,6 +138,7 @@ module rocketchip_top(
 
   `axi_out_interface(M_AXI_MEM, io_mem_axi_0_),
   `axi_out_interface(M_AXI_MMIO, axi_uart_),
+  `axi_in_interface(M_AXI_CDMA, axi_cdma_),
   input   io_interrupts_0,
   input   io_interrupts_1,
   output  io_debug_req_ready,
@@ -151,16 +152,17 @@ module rocketchip_top(
   output [33:0] io_debug_resp_bits_data
 );
 
-ExampleTop top(
+ExampleRocketTop top(
    .clock(uncoreclk),
    .reset(uncorerst),
    .io_coreclk(coreclk),
    .io_corerst(corerst),
-   .io_interrupts_0(io_interrupts_0),
-   .io_interrupts_1(io_interrupts_1),
+   .io_interrupts_0_0(io_interrupts_0),
+   .io_interrupts_0_1(io_interrupts_1),
 
-   `axi_connect_interface(io_mem_axi_0_, io_mem_axi_0_),
-   `axi_connect_interface(io_mmio_axi_0_, axi_uart_),
+   `axi_connect_interface(io_mem_axi4_0_, io_mem_axi_0_),
+   `axi_connect_interface(io_mmio_axi4_0_, axi_uart_),
+   `axi_connect_interface(io_l2_axi4_0_, axi_cdma_),
 
    .io_debug_req_ready(io_debug_req_ready),
    .io_debug_req_valid(io_debug_req_valid),
