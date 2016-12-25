b6159209fd53e6a363d757a25914c19ed76fd0f9
yzh <yuzihao@ict.ac.cn>
2016-12-25 19:59:51 +0800

    srcs,rtl,{migcp,cdma_addr}: use axi bundle definitions

--- srcs/rtl/control_plane/mig/control_plane.v
+++ srcs/rtl/control_plane/mig/control_plane.v
@@ -48,8 +48,8 @@ module mig_control_plane # (
 	output trigger_axis_tvalid,
 	output [15:0] trigger_axis_tdata,
 
-   `axi_in_interface(S_AXI, s_axi),
-   `axi_out_interface(M_AXI, m_axi)
+   `axi_in_interface(S_AXI, s_axi, 5),
+   `axi_out_interface(M_AXI, m_axi, 5)
 );
 
   localparam P_DATA_WIDTH = 64;
@@ -137,12 +137,12 @@ module mig_control_plane # (
         .DATA_MASK(DATA_MASK),
         .DATA_OFFSET(DATA_OFFSET),
        
-        .TAG_A    (s_axi_awuser),
+        .TAG_A    (s_axi_aw_bits_user),
         .DO_A     (ptable_do_a),
         .TAG_MATCH_A(w_match),
 
         /* AR channel */
-        .TAG_B    (s_axi_aruser),
+        .TAG_B    (s_axi_ar_bits_user),
         .DO_B     (ptable_do_b),
         .TAG_MATCH_B(r_match),
         .APM_DATA(APM_DATA),
@@ -159,44 +159,44 @@ module mig_control_plane # (
     wire [31:0] w_limit = ptable_do_a[63:32];
     wire [31:0] r_limit = ptable_do_b[63:32];
     
-    assign  m_axi_awaddr = {32{w_match}} & ((s_axi_awaddr & ~w_limit) | w_base);
-    assign  m_axi_araddr = {32{r_match}} & ((s_axi_araddr & ~r_limit) | r_base);
-    assign  m_axi_arburst = s_axi_arburst;
-    assign  m_axi_arcache = s_axi_arcache;
-    assign  m_axi_arid    = s_axi_arid   ;
-    assign  m_axi_arlen   = s_axi_arlen  ;
-    assign  m_axi_arlock  = s_axi_arlock ;
-    assign  m_axi_arprot  = s_axi_arprot ;
-    assign  s_axi_arready = m_axi_arready;
-    assign  m_axi_arsize  = s_axi_arsize ;
-    assign  m_axi_arvalid = s_axi_arvalid;
-    assign  m_axi_awburst = s_axi_awburst;
-    assign  m_axi_awcache = s_axi_awcache;
-    assign  m_axi_awid    = s_axi_awid   ;
-    assign  m_axi_awlen   = s_axi_awlen  ;
-    assign  m_axi_awlock  = s_axi_awlock ;
-    assign  m_axi_awprot  = s_axi_awprot ;
-    assign  s_axi_awready = m_axi_awready;
-    assign  m_axi_awsize  = s_axi_awsize ;
-    assign  m_axi_awvalid = s_axi_awvalid;
-    assign  s_axi_bid     = m_axi_bid    ;
-    assign  m_axi_bready  = s_axi_bready ;
-    assign  s_axi_bresp   = m_axi_bresp  ;
-    assign  s_axi_bvalid  = m_axi_bvalid ;
-    assign  s_axi_rdata   = m_axi_rdata  ;
-    assign  s_axi_rid     = m_axi_rid    ;
-    assign  s_axi_rlast   = m_axi_rlast  ;
-    assign  m_axi_rready  = s_axi_rready ;
-    assign  s_axi_rresp   = m_axi_rresp  ;
-    assign  s_axi_rvalid  = m_axi_rvalid ;
-    assign  m_axi_wdata   = s_axi_wdata  ;
-    assign  m_axi_wlast   = s_axi_wlast  ;
-    assign  s_axi_wready  = m_axi_wready ;
-    assign  m_axi_wstrb   = s_axi_wstrb  ;
-    assign  m_axi_wvalid  = s_axi_wvalid ;
-    assign  m_axi_arqos   = s_axi_arqos  ;
-    assign  m_axi_aruser  = s_axi_aruser ;
-    assign  m_axi_awqos   = s_axi_awqos  ;
-    assign  m_axi_awuser  = s_axi_awuser ;
+    assign  m_axi_aw_bits_addr = {32{w_match}} & ((s_axi_aw_bits_addr & ~w_limit) | w_base);
+    assign  m_axi_ar_bits_addr = {32{r_match}} & ((s_axi_ar_bits_addr & ~r_limit) | r_base);
+    assign  m_axi_ar_bits_burst = s_axi_ar_bits_burst;
+    assign  m_axi_ar_bits_cache = s_axi_ar_bits_cache;
+    assign  m_axi_ar_bits_id    = s_axi_ar_bits_id   ;
+    assign  m_axi_ar_bits_len   = s_axi_ar_bits_len  ;
+    assign  m_axi_ar_bits_lock  = s_axi_ar_bits_lock ;
+    assign  m_axi_ar_bits_prot  = s_axi_ar_bits_prot ;
+    assign  s_axi_ar_ready = m_axi_ar_ready;
+    assign  m_axi_ar_bits_size  = s_axi_ar_bits_size ;
+    assign  m_axi_ar_valid = s_axi_ar_valid;
+    assign  m_axi_aw_bits_burst = s_axi_aw_bits_burst;
+    assign  m_axi_aw_bits_cache = s_axi_aw_bits_cache;
+    assign  m_axi_aw_bits_id    = s_axi_aw_bits_id   ;
+    assign  m_axi_aw_bits_len   = s_axi_aw_bits_len  ;
+    assign  m_axi_aw_bits_lock  = s_axi_aw_bits_lock ;
+    assign  m_axi_aw_bits_prot  = s_axi_aw_bits_prot ;
+    assign  s_axi_aw_ready = m_axi_aw_ready;
+    assign  m_axi_aw_bits_size  = s_axi_aw_bits_size ;
+    assign  m_axi_aw_valid = s_axi_aw_valid;
+    assign  s_axi_b_bits_id     = m_axi_b_bits_id    ;
+    assign  m_axi_b_ready  = s_axi_b_ready ;
+    assign  s_axi_b_bits_resp   = m_axi_b_bits_resp  ;
+    assign  s_axi_b_valid  = m_axi_b_valid ;
+    assign  s_axi_r_bits_data   = m_axi_r_bits_data  ;
+    assign  s_axi_r_bits_id     = m_axi_r_bits_id    ;
+    assign  s_axi_r_bits_last   = m_axi_r_bits_last  ;
+    assign  m_axi_r_ready  = s_axi_r_ready ;
+    assign  s_axi_r_bits_resp   = m_axi_r_bits_resp  ;
+    assign  s_axi_r_valid  = m_axi_r_valid ;
+    assign  m_axi_w_bits_data   = s_axi_w_bits_data  ;
+    assign  m_axi_w_bits_last   = s_axi_w_bits_last  ;
+    assign  s_axi_w_ready  = m_axi_w_ready ;
+    assign  m_axi_w_bits_strb   = s_axi_w_bits_strb  ;
+    assign  m_axi_w_valid  = s_axi_w_valid ;
+    assign  m_axi_ar_bits_qos   = s_axi_ar_bits_qos  ;
+    assign  m_axi_ar_bits_user  = s_axi_ar_bits_user ;
+    assign  m_axi_aw_bits_qos   = s_axi_aw_bits_qos  ;
+    assign  m_axi_aw_bits_user  = s_axi_aw_bits_user ;
 
 endmodule
