1b36e0806ab14bbe1d73c4348b39ee2d2eca7dbf
Wang Huizhe <huizhewang@outlook.com>
2017-04-18 17:13:02 +0800

    uncore,pard: use width induction to simplify code
    Diffed generated code with original one, all the same.

--- src/main/scala/uncore/pard/TokenBucket.scala
+++ src/main/scala/uncore/pard/TokenBucket.scala
@@ -13,21 +13,18 @@ class TokenBucket(implicit p: Parameters) extends Module {
     val enable = Output(Bool())
   })
 
-  val sizeBits = p(BucketBits).size
-  val freqBits = p(BucketBits).freq
-
-  val nTokensNext = Wire(UInt(sizeBits.W))
+  val nTokensNext = Wire(UInt())
   val nTokens = RegNext(
     Mux(nTokensNext < io.bucket.size, nTokensNext, io.bucket.size),
     io.bucket.size)
 
-  val counterNext = Wire(UInt(freqBits.W))
-  val counter = RegNext(counterNext, 0.U(freqBits.W))
-  counterNext := Mux(counter >= io.bucket.freq, 1.U(freqBits.W), counter + 1.U)
+  val counterNext = Wire(UInt())
+  val counter = RegNext(counterNext, 0.U(p(BucketBits).freq.W))
+  counterNext := Mux(counter >= io.bucket.freq, 1.U, counter + 1.U)
 
-  val tokenAdd = Mux(counter === 1.U(freqBits.W), io.bucket.inc, 0.U)
+  val tokenAdd = Mux(counter === 1.U, io.bucket.inc, 0.U)
 
-  val bypass = io.bucket.freq === 0.U(freqBits.W)
+  val bypass = io.bucket.freq === 0.U
   val channelEnables = List(io.read, io.write).map { ch => ch.valid && ch.ready }
   val rTokenSub :: wTokenSub :: _ = List(io.read, io.write).zip(channelEnables).map {
     case (ch, en) => Mux(en && !bypass, ch.bits, 0.U)
@@ -41,7 +38,7 @@ class TokenBucket(implicit p: Parameters) extends Module {
   nTokensNext := nTokens + tokenAdd - rTokenSubReal - wTokenSubReal
 
   // Throttle logic
-  val threshold = RegInit(0.U(sizeBits.W))
+  val threshold = RegInit(0.U)
   val enable = RegInit(false.B)
   when (channelEnables.reduce(_ || _) && consumeUp) {
     enable := false.B
