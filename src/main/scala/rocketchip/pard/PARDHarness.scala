1d9214b7077606dd56bb6fc34a7ab88e85134d59
Zihao Yu <yuzihao@ict.ac.cn>
2017-06-27 10:03:13 +0800

    rocketchip,TestHarness: use methods to connect/disconnect intr and mem

--- src/main/scala/rocketchip/pard/PARDHarness.scala
+++ src/main/scala/rocketchip/pard/PARDHarness.scala
@@ -27,27 +27,12 @@ class TestHarness()(implicit p: Parameters) extends Module {
   }
   val dut = Module(LazyModule(new PARDSimTop).module)
 
-  dut.interrupts := UInt(0)
   dut.debug.clockeddmi.get.dmi.req.valid := Bool(false)
   dut.debug.clockeddmi.get.dmi.resp.ready := Bool(true)
 
   // Make cores always runnable
   dut.L1enable.foreach(_ := io.en)
   dut.trafficGeneratorEnable := Bool(false)
-
-  val channels = p(coreplex.BankedL2Config).nMemoryChannels
-  if (channels > 0) Module(LazyModule(new SimAXIMem(channels)).module).io.axi4 <> dut.mem_axi4
-    for (axi4 <- dut.mem_axi4) {
-      when(axi4.ar.valid) {
-  //      printf("axi4.ar.user = %x, axi4.ar.addr = %x\n", axi4.ar.bits.user, axi4.ar.bits.addr)
-      }
-      when(axi4.aw.valid) {
-   //     printf("axi4.aw.user = %x, axi4.aw.addr = %x\n", axi4.aw.bits.user, axi4.aw.bits.addr)
-      }
-      // We can have traffic generator with different dsid here.
-      // assert(!axi4.ar.valid || axi4.ar.bits.user === UInt(0x1, width = 16))
-      // assert(!axi4.aw.valid || axi4.aw.bits.user === UInt(0x1, width = 16))
-    }
 
   io.success := Bool(false)
 
@@ -64,5 +49,7 @@ class TestHarness()(implicit p: Parameters) extends Module {
   mmio_axi4.aw.ready := Bool(true)
   mmio_axi4.w.ready := Bool(true)
 
+  dut.tieOffInterrupts()
+  dut.connectSimAXIMem()
   dut.tieOffAXI4SlavePort()
 }
