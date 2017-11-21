0fc9d385c50068779ad072034b1172b39f526314
Zihao Yu <yuzihao@ict.ac.cn>
2017-11-21 10:39:16 +0800

    rocket-chip,PARDConfigs: use different address base for different boards

--- src/main/scala/rocketchip/pard/PARDConfigs.scala
+++ src/main/scala/rocketchip/pard/PARDConfigs.scala
@@ -22,6 +22,7 @@ class PARDFPGAConfigvc709 extends Config(new FPGAConfig
   //++ new WithJtagDTM
   ++ new WithoutFPU
   ++ new WithExtMemSize(0x40000000L) // 1GB
+  ++ new WithAddressMapperBase(0x0L)
   ++ new WithNBigCores(2)
   ++ new WithAynchronousRocketTiles(8, 3)
   ++ new WithRTCPeriod(50) // gives 10 MHz RTC assuming 50 MHz uncore clock
@@ -32,6 +33,7 @@ class PARDFPGAConfigzedboard extends Config(new FPGAConfig
   //++ new WithJtagDTM
   ++ new WithoutFPU
   ++ new WithExtMemSize(0x4000000L) // 64MB
+  ++ new WithAddressMapperBase(0x10000000L) // 256MB
   ++ new WithNBigCores(2)
   ++ new WithAynchronousRocketTiles(8, 3)
   ++ new WithRTCPeriod(50) // gives 10 MHz RTC assuming 50 MHz uncore clock
@@ -42,6 +44,7 @@ class PARDFPGAConfigzcu102 extends Config(new FPGAConfig
   //++ new WithJtagDTM
   ++ new WithoutFPU
   ++ new WithExtMemSize(0x10000000L) // 256MB
+  ++ new WithAddressMapperBase(0x0L)
   ++ new WithNBigCores(2)
   ++ new WithAynchronousRocketTiles(8, 3)
   ++ new WithRTCPeriod(100)
