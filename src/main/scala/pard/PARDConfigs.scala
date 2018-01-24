c98e37d4f5ec3c4b4350de66bc784f3178959687
Zihao Yu <yuzihao@ict.ac.cn>
2018-01-24 11:39:00 +0800

    board,zedboard,prm: add axi jtag

--- src/main/scala/pard/PARDConfigs.scala
+++ src/main/scala/pard/PARDConfigs.scala
@@ -24,7 +24,7 @@ class PARDSimConfig extends Config(
 class PARDFPGAConfigzedboard extends Config(
   new WithBlockingL1
   ++ new WithoutFPU
-  //++ new WithJtagDTM
+  ++ new WithJtagDTM
   ++ new WithExtMemSize(0x80000000L)
 //  ++ new WithAddressMapperBase(0x10000000L) // 256MB
   ++ new WithNCores(4)
