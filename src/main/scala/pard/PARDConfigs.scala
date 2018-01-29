96f086e919a113aa209a508f39e5af637ce5e649
Zihao Yu <yuzihao@ict.ac.cn>
2018-01-29 15:15:23 +0800

    pard,PARDConfigs: remove unused code

--- src/main/scala/pard/PARDConfigs.scala
+++ src/main/scala/pard/PARDConfigs.scala
@@ -15,12 +15,9 @@ class PARDSimConfig extends Config(
   ++ new WithoutFPU
   ++ new WithNoHype
   ++ new WithNCores(2)
-//  ++ new WithAynchronousRocketTiles(8, 3)
   ++ new WithExtMemSize(0x2000000L) // 32MB
   ++ new WithL2Capacity(128)
   ++ new WithNL2Ways(16)
-//  ++ new WithRTCPeriod(5)
-//  ++ new DefaultConfig)
   ++ new DefaultL2Config)
 
 class PARDFPGAConfigzedboard extends Config(
@@ -28,17 +25,13 @@ class PARDFPGAConfigzedboard extends Config(
   ++ new WithoutFPU
   ++ new WithJtagDTM
   ++ new WithExtMemSize(0x80000000L)
-//  ++ new WithAddressMapperBase(0x10000000L) // 256MB
   ++ new WithNCores(4)
   ++ new WithNBtbEntry(0)
   ++ new WithL1ICacheWays(2)
   ++ new WithL1DCacheWays(2)
-//  ++ new WithAynchronousRocketTiles(8, 3)
   ++ new WithRTCPeriod(4) // gives 10 MHz RTC assuming 40 MHz uncore clock
-//  ++ new BucketConfig
   ++ new WithL2Capacity(256)
   ++ new WithNL2Ways(16)
-  ++ new WithPLRU
   ++ new DefaultL2FPGAConfig
 )
 
@@ -47,13 +40,9 @@ class PARDFPGAConfigzcu102 extends Config(
   ++ new WithoutFPU
   ++ new WithJtagDTM
   ++ new WithExtMemSize(0x80000000L)
-//  ++ new WithAddressMapperBase(0x0L)
   ++ new WithNCores(1)
-//  ++ new WithAynchronousRocketTiles(8, 3)
   ++ new WithRTCPeriod(8) // gives 10 MHz RTC assuming 80 MHz uncore clock
-//  ++ new BucketConfig
   ++ new WithL2Capacity(2048)
   ++ new WithNL2Ways(16)
-  ++ new WithPLRU
   ++ new DefaultL2FPGAConfig
 )
