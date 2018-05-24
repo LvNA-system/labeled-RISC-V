9bd5441d0b0f5a7d598abdded28af892823228b9
Zihao Yu <yuzihao@ict.ac.cn>
2018-05-24 23:05:34 +0800

    lvna: add uart while running emu

--- src/main/scala/lvna/LvNATop.scala
+++ src/main/scala/lvna/LvNATop.scala
@@ -1,21 +1,19 @@
 // See LICENSE.SiFive for license details.
 
-package freechips.rocketchip.pard
+package freechips.rocketchip.system
 
 import Chisel._
-import freechips.rocketchip.config._
-import freechips.rocketchip.subsystem._
-import freechips.rocketchip.system._
+import freechips.rocketchip.config.Parameters
 
 import sifive.blocks.devices.uart._
 
-class PARDSimTop(implicit p: Parameters) extends ExampleRocketSystem
+class LvNAEmuTop(implicit p: Parameters) extends ExampleRocketSystem
     with HasPeripheryUART
 
-class PARDSimTopModule[+L <: PARDSimTop](_outer: L) extends ExampleRocketSystemModuleImp(_outer)
+class LvNAEmuTopModule[+L <: LvNAEmuTop](_outer: L) extends ExampleRocketSystemModuleImp(_outer)
     with HasPeripheryUARTModuleImp
 
-class PARDFPGATop(implicit p: Parameters) extends ExampleRocketSystem
+class LvNAFPGATop(implicit p: Parameters) extends ExampleRocketSystem
 
-class PARDFPGATopModule[+L <: PARDFPGATop](_outer: L) extends ExampleRocketSystemModuleImp(_outer)
+class LvNAFPGATopModule[+L <: LvNAFPGATop](_outer: L) extends ExampleRocketSystemModuleImp(_outer)
 
