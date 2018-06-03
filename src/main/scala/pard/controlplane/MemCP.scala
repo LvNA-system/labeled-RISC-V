4ebedbf5468f294d865c995f1e65c2dec282531d
Zihao Yu <yuzihao@ict.ac.cn>
2018-06-03 09:46:33 +0800

    pard,controlplane,MemCP: do not enable token bucket by default in emulation

--- src/main/scala/pard/controlplane/MemCP.scala
+++ src/main/scala/pard/controlplane/MemCP.scala
@@ -47,7 +47,7 @@ class MemControlPlaneModule(implicit p: Parameters) extends ControlPlaneModule {
     for (i <- 0 until nTiles) {
       if (p(UseSim)) {
         sizeRegs(i) := 32.U
-        freqRegs(i) := 128.U
+        freqRegs(i) := 0.U
         incRegs(i) := 1.U
       }
       else {
