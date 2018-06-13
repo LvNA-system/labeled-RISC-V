7fd0b7a81683e5221275a9c87b2e4192e63799d9
Zihao Yu <yuzihao@ict.ac.cn>
2018-06-13 18:19:29 +0800

    pard,PARDConfigs: use 16MB memory for emu

--- emulator/meminit.sh
+++ emulator/meminit.sh
@@ -2,6 +2,6 @@
 # usage: this-script <verilog-file> <mem-size> <bin-file>
 
 #append="initial \$readmemh(\"$3\", mem, 'h0);"
-append="initial begin \$readmemh(\"$3\", mem, 'h0); \$readmemh(\"$3\", mem, 'h200000); end"
+append="initial begin \$readmemh(\"$3\", mem, 'h0); \$readmemh(\"$3\", mem, 'h100000); end"
 
 sed -i -e "/reg \[63:0\] mem \[0:`expr $2 / 8 - 1`\];/a $append" $1
