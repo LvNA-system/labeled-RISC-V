e6e8b04fa86c3bafad240cf9014db03caf2e2f2d
Zihao Yu <yuzihao@ict.ac.cn>
2018-06-25 09:49:13 +0800

    fpga,board,zcu102: change axi id bits to 4 to match rocketchip project

--- fpga/board/zcu102/rtl/system_top.v
+++ fpga/board/zcu102/rtl/system_top.v
@@ -4,10 +4,10 @@ module system_top (
   output [7:0] led
 );
 
-  `axi_wire(AXI_MEM_MAPPED, 64, 1);
-  `axi_wire(AXI_MEM, 64, 1);
+  `axi_wire(AXI_MEM_MAPPED, 64, 4);
+  `axi_wire(AXI_MEM, 64, 4);
   `axilite_wire(AXILITE_MMIO);
-  `axi_wire(AXI_DMA, 64, 1);
+  `axi_wire(AXI_DMA, 64, 4);
 
   wire jtag_TCK;
   wire jtag_TMS;
