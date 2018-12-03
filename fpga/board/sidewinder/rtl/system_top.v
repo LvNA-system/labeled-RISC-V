31bfb283eec8a308f21ca73cc4d525bb3168362b
Wang Huizhe <huizhewang@outlook.com>
2018-12-03 14:27:41 +0800

    fbus: update block design for sidewinder

--- fpga/board/sidewinder/rtl/system_top.v
+++ fpga/board/sidewinder/rtl/system_top.v
@@ -9,6 +9,9 @@ module system_top (
   `axilite_wire(AXILITE_MMIO);
   `axi_wire(AXI_DMA, 64, 1);
 
+  `axi_wire(AXI_FBUS_FROM_ZYNQ, 64, 16);
+  `axi_wire(AXI_FBUS_TO_ROCKET, 64, 16);
+
   wire jtag_TCK;
   wire jtag_TMS;
   wire jtag_TDI;
@@ -26,6 +29,7 @@ module system_top (
   zynq_soc zynq_soc_i (
     `axi_connect_if(S_AXI_MEM, AXI_MEM_MAPPED),
     `axilite_connect_if(S_AXILITE_MMIO, AXILITE_MMIO),
+    `axi_connect_if(M_AXI_HPM0_FPD_0, AXI_FBUS_FROM_ZYNQ),
     `axi_connect_if_no_id(M_AXI_DMA, AXI_DMA),
 
     .jtag_TCK(jtag_TCK),
@@ -49,9 +53,15 @@ module system_top (
     `axi_connect_if(m_axi, AXI_MEM_MAPPED)
   );
 
+  dma_addr_mapper dma_addr_mapper_i(
+    `axi_connect_if(s_axi, AXI_FBUS_FROM_ZYNQ),
+    `axi_connect_if(m_axi, AXI_FBUS_TO_ROCKET)
+  );
+
   pardcore pardcore_i(
     `axi_connect_if(M_AXI_MEM, AXI_MEM),
     `axi_connect_if(S_AXI_DMA, AXI_DMA),
+    `axi_connect_if(S_AXI_SBUS, AXI_FBUS_TO_ROCKET),
     `axilite_connect_if(M_AXILITE_MMIO, AXILITE_MMIO),
 
     .jtag_TCK(jtag_TCK),
