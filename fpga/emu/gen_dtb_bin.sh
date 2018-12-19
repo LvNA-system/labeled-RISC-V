4927b5aca0d592bcdcf433ad4950849c19304acc
Wang Huizhe <wanghuizhe@ict.ac.cn>
2018-12-19 16:13:33 +0800

    emu: split dts for nohype when make (require pyfdt)

--- fpga/emu/gen_dtb_bin.sh
+++ fpga/emu/gen_dtb_bin.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/env bash
-
-if [ ! $1 ]
-then
-    echo "Please specify the device tree source name (without extension)"
-    exit
-fi
-
-SCRIPT=$(realpath $0)
-BUILD_ROOT=$(dirname $SCRIPT)/../build
-DTS=$BUILD_ROOT/$1.dts
-DTB=$BUILD_ROOT/$1.dtb
-TXT=$DTB.txt
-dtc $DTS -o $DTB
-sh gen_bin.sh $DTB $TXT
+BUILD_DIR=$1
+for i in $(ls $1/c*.dtb)
+do
+    sh gen_bin.sh $i $i.txt
+done
